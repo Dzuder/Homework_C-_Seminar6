094a2c42457a5c3051fe6ff5958db707f4e72d9b
Dzuder <dzuder14@yandex.ru>
2023-01-24 18:03:34 +0500

    ended homework

---
+++ Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+Clear();
+WriteLine("Введите числа через пробел (хоть сколько, хоть отрицательные: ");
+string numbersEnteredUser = ReadLine()!;
+string[] parameter = numbersEnteredUser.Split(" ", System.StringSplitOptions.RemoveEmptyEntries);
+
+
+
+int[] array = GetArray(parameter);
+WriteLine($"Количество чисел введенные вами которые больше нуля:  {CheckNumbers(array)}.");
+
+
+
+int[] GetArray(string[] par)
+{
+    int[] arrayUser = new int[par.Length];
+    for (int i = 0; i < par.Length; i++)
+    {
+        arrayUser[i] = int.Parse(parameter[i]);
+    }
+    return arrayUser;
+}
+
+int CheckNumbers(int[] arr1)
+{
+    int counter = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i] > 0) counter++;
+    }
+    return counter;
+}
---
+++ Task43/Program.cs
@@ -0,0 +1,36 @@
+﻿using static System.Console;
+Clear();
+
+// Напишите программу, которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+
+WriteLine("Эта программа, найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2\n \nДля этого нужно ввести значения");
+
+
+WriteLine("Введите значение k1:");
+double k1 = double.Parse(ReadLine()!);
+WriteLine("Введите значение b1:");
+double b1 = double.Parse(ReadLine()!);
+WriteLine("Введите значение k2:");
+double k2 = double.Parse(ReadLine()!);
+WriteLine("Введите значение b2:");
+double b2 = double.Parse(ReadLine()!);
+
+
+
+double x = XCoordinate(k1, b1, k2, b2);
+WriteLine((k1 == k2 && b1 == b2) ?
+"Эти прямые совпадают!!"
+ : k1 == k2 ? "Эти прямые параллельны!!!"
+ : $"Координаты точки пересечения этих прямых ({Math.Round(x, 2)}; {Math.Round((k1 * x + b1), 2)}) ");
+
+
+
+
+
+double XCoordinate(double a1, double c1, double a2, double c2)
+{
+    double res = (c2 - c1) / (a1 - a2);
+    return res;
+}
